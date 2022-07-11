72fdb73069c7312ee0cb8f1292efec7d8d9dfdc5
Kushagra Madhukar <madhukarkushagra@gmail.com>
2022-07-12 04:02:38 +0530

    Create deepSearchCount.js

---
+++ miscellaneous/deepSearchCount.js
@@ -0,0 +1,26 @@
+
+/* 
+Create a function that returns the number of all possible item types matching a given condition passed as callback. The condition should be checked for all items nested in any array
+It should
+Input:
+const arr = [[1, [2, [3, 4, "foo", { a: 1, b: 2 }]], "bar"]];
+const count = countInArray(arr, (e) => typeof e === "number");
+console.log(count);
+
+Output:
+4 */
+
+// We can also find the number of arrays in the array with this
+
+const deepSearcher = (arr, test) => {
+  let count = test(arr) ? 1 : 0;
+  if(!Array.isArray(arr)) return count;
+  for(let ele of arr) {
+    if(Array.isArray(ele)) count += deepSearcher(ele, test);
+    else if(test(ele)) count++;
+  }
+  return count;
+}
+const arrx = [[1, [2, [3, 4, "foo", { a: 1, b: 2 }]], "bar"]];
+console.log(deepSearcher(arrx, (e) => typeof e === "number"));
+console.log(deepSearcher(arrx, (e) => e instanceof Array));
