3dbd6902ed6ebdf060fef88586cbc3d06946d13b
Kushagra Madhukar <madhukarkushagra@gmail.com>
2022-07-13 03:18:19 +0530

    Create chain.js

---
+++ miscellaneous/chain.js
@@ -0,0 +1,15 @@
+/**
+ * chain(['a', 'b', 'c', 'd'], 2)  => [['a', 'b'], ['c', 'd']]
+ * chain(['a', 'b', 'c', 'd', 'e'], 3) => [['a', 'b', 'c'], ['d', 'e']]
+ */
+
+function chain(arr, linkLength) {
+   let chainArr = [];
+   let unprocessedArr = [...arr];
+   while(unprocessedArr.length) {
+     chainArr.push(unprocessedArr.slice(0, linkLength));
+     unprocessedArr.splice(0, linkLength)
+   }
+   return chainArr;
+}
+console.log(chain(['a', 'b', 'c', 'd', 'e', 'f'], 2));
