a48e28076294cc559c79f80c79d1b2a9d32042ca
Kushagra Madhukar <madhukarkushagra@gmail.com>
2022-07-15 02:45:08 +0530

    Update debounce.js

--- miscellaneous/debounce.js
+++ miscellaneous/debounce.js
@@ -9,3 +9,11 @@ const debounce = (fn, delay) => {
       }, delay);
  }
 }
+
+function debounce(func, delay = 300){
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => { func.apply(this, args); }, delay);
+  };
+}
